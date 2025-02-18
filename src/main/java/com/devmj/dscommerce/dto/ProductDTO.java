bd82c2d38d0869edec2e19717be1765d96f092da
Marco Júnior <mrcez4rr@gmail.com>
2025-02-18 10:10:46 -0300

    Fix Validation

--- src/main/java/com/devmj/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/devmj/dscommerce/dto/ProductDTO.java
@@ -8,6 +8,7 @@ import com.devmj.dscommerce.entities.Product;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 
@@ -19,10 +20,10 @@ public class ProductDTO {
 	@Size(min=3, max=80, message="Nome precisa ter de 3 a 80 caracteres")
 	@NotBlank(message= "Campo requerido")
 	private String name;
-	
 	@Size(min=10, message="Descrição precisa ter no mínimo 10 caracteres")
 	@NotBlank(message= "Campo Requerido")
 	private String description;
+	@NotNull(message = "Campo requerido!")
 	@Positive(message="O preço deve ser positivo")
 	private Double price;
 	private String imgUrl;
